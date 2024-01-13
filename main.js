f6b4c034fd13445b40b375f077ef2a22b005b0f7
Web Design Mastery <130627020+WebDesignMastery@users.noreply.github.com>
2024-01-13 16:28:02 +0530

    initial commit

---
+++ main.js
@@ -0,0 +1,62 @@
+const navBtn = document.getElementById("menu-btn");
+const navLinks = document.getElementById("nav-links");
+
+navBtn.addEventListener("click", (e) => {
+  if (navLinks.classList.contains("open")) {
+    navLinks.classList.add("close");
+    navLinks.addEventListener(
+      "animationend",
+      (e) => {
+        navLinks.classList.remove("close");
+        navLinks.classList.remove("open");
+      },
+      { once: true }
+    );
+  } else {
+    navLinks.classList.add("open");
+  }
+});
+
+navLinks.addEventListener("click", (e) => {
+  navLinks.classList.add("close");
+  navLinks.addEventListener(
+    "animationend",
+    (e) => {
+      navLinks.classList.remove("close");
+      navLinks.classList.remove("open");
+    },
+    { once: true }
+  );
+});
+
+const explore = document.querySelector(".explore__wrapper-inner");
+
+const exploreContent = Array.from(explore.children);
+
+exploreContent.forEach((item) => {
+  const duplicateNode = item.cloneNode(true);
+  duplicateNode.setAttribute("aria-hidden", true);
+  explore.appendChild(duplicateNode);
+});
+
+const swiper = new Swiper(".swiper", {
+  slidesPerView: "auto",
+  spaceBetween: 20,
+});
+
+const faq = document.querySelector(".faq__grid");
+
+faq.addEventListener("click", (e) => {
+  const target = e.target;
+  const faqCard = target.closest(".faq__card");
+  if (target.classList.contains("ri-arrow-down-s-line")) {
+    if (faqCard.classList.contains("active")) {
+      faqCard.classList.remove("active");
+    } else {
+      Array.from(faq.children).forEach((item) => {
+        item.classList.remove("active");
+      });
+      faqCard.classList.add("active");
+    }
+  }
+});
